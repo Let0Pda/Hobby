881c7a81d5c8160c8075b0f46eeb0cd9909328a0
Sourcery AI <>
2023-08-30 09:48:01 +0000

    'Refactored by Sourcery'

--- MinMaxValue/python/app.py
+++ MinMaxValue/python/app.py
@@ -2,15 +2,12 @@ array = [1, 5, 4, 3, 6, 7, 0]
 
 posMin = 0
 posMax = 0
-index = 0
 size = len(array)
 
-while index < size:
+for index in range(size):
     if array[index] > array[posMax]:
         posMax = index
 
     if array[index] < array[posMin]:
         posMin = index
-    index += 1
-
 print(f"posMax: {posMax} posMin: {posMin}")
